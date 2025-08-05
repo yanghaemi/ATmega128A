550bc57c57930aa9935d893a2417e20ed0ee24d7
Yang Haemi <107767152+yanghaemi@users.noreply.github.com>
2025-08-05 17:44:14 +0900

    Create define.h

---
+++ buzzer+adc+uart/define.h
@@ -0,0 +1,26 @@
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+// clock
+#define F_CPU 16000000UL
+#define BAUD 9600
+
+
+//
+#define L_DO 523
+#define L_RE 587
+#define FA_ 740
+#define SOL 784
+#define RA 880
+#define TI 988
+#define DO 1047
+#define DO_ 1109
+#define RE 1175
+#define RE_ 1245
+#define MI 1319
+#define FA 1397
+#define H_SOL 1568
+
+
+
+#endif /* DEFINE_H */
